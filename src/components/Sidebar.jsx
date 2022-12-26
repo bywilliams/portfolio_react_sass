ffe90464065f9b9ab7eb445e03099e47de3630db
bywilliams <willian.seu@gmail.com>
2022-12-26 20:25:39 -0300

    estrutura e formatacao do sidebar e mainContent

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,19 @@
+import Avatar from "../img/wil.jpeg";
+
+import  "../styles/components/sidebar.sass";
+
 const Sidebar = () =>{
-    return <aside id="sidebar">
-        <p>Imagem</p>
-        <p className="title">Desenvolvedor Front-End</p>
-        <p>Redes Sociais</p>
-        <p>Informações de contato</p>
-        <a href="#" className="btn">Download CV</a>
-    </aside>;
+    return (
+        <aside id="sidebar">
+            <img src={Avatar} alt="William Silva" title="wil jpeg" />
+            <p className="title">Desenvolvedor Front-End</p>
+            <p>Redes Sociais</p>
+            <p>Informações de contato</p>
+            <a href="#" className="btn">
+                Download CV
+            </a>
+        </aside>
+    );
 }
 
 export default Sidebar;
